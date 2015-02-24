0df0c7312693f8dd92bb24d56191c6e1118582ca
Salvador de la Puente <salva@unoyunodiez.com>
2015-02-24 17:38:30 +0100

    Removing worker communication from master. It remains in concept branch

--- offline-cache-setup.js
+++ offline-cache-setup.js
@@ -1,27 +1,11 @@
-
 (function () {
   'use strict';
 
   var root = document.currentScript.dataset.root || '/';
-  var channel = new MessageChannel();
-
-  channel.port1.onmessage = function (message) {
-    var loglevel = message.data.loglevel;
-    if (loglevel !== 'undefined') {
-      var args = message.data.args || [];
-      console[loglevel].apply(console, args);
-    }
-  };
 
   navigator.serviceWorker.register(root + 'offline-cache.js', {
     scope: root
-  }).then(function (registration) {
-    var sw = registration.active ||
-             registration.waiting ||
-             registration.installing;
-    sw.postMessage('subscribe', [channel.port2]);
-  }, function (reason) {
+  }).catch(function (reason) {
     console.log(reason);
   });
 }());
-
