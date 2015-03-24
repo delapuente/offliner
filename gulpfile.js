81676ac37535f93ba716d3290dea4f6c1bb96780
Salvador de la Puente <salva@unoyunodiez.com>
2015-03-24 18:18:46 +0100

    Minifying offliner

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 
 var gulp = require('gulp');
 var yuidoc = require('gulp-yuidoc');
+var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var webserver = require('gulp-webserver');
 var jshint = require('gulp-jshint');
@@ -10,8 +11,9 @@ var watch = require('gulp-watch');
 
 gulp.task('dist', function() {
   gulp.src('src/offliner.js')
-      .pipe(uglify({outSourceMap: './dist/offliner.js.map'}))
-      .pipe(gulp.dest('./dist/offliner.min.js'));
+      .pipe(uglify())
+      .pipe(rename('offliner.min.js'))
+      .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('docs', function() {
